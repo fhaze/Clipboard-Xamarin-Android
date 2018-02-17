41d61396dec3d3345b6b25af2ca980312439f0e4
FHaze <eder_arm@icloud.com>
2018-02-17 13:11:13 -0200

    First commit

---
+++ ClipboardTest/ClipboardTest.Android/Services/ClipboardCaptureService.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace ClipboardTest.Droid.Services
+{
+    [Service]
+    public class ClipboardCaptureService : Service
+    {
+        private ClipboardManager _clipboardManager;
+
+        public override IBinder OnBind(Intent intent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+            _clipboardManager = GetSystemService(ClipboardService) as ClipboardManager;
+            _clipboardManager.AddPrimaryClipChangedListener(new TestListener(this, _clipboardManager));
+
+            return base.OnStartCommand(intent, flags, startId);
+        }
+    }
+}
---
+++ ClipboardTest/ClipboardTest.Android/Services/TestListener.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Widget;
+
+namespace ClipboardTest.Droid.Services
+{
+    public class TestListener : Java.Lang.Object, ClipboardManager.IOnPrimaryClipChangedListener
+    {
+        private readonly Service          _service;
+        private readonly ClipboardManager _clipboardManager;
+
+        public TestListener(Service service, ClipboardManager clipboardManager)
+        {
+            _service          = service;
+            _clipboardManager = clipboardManager;
+        }
+
+        public void OnPrimaryClipChanged()
+        {
+            var clipData = _clipboardManager.PrimaryClip;
+            Toast.MakeText(_service, clipData.GetItemAt(0).Text, ToastLength.Long).Show();
+        }
+    }
+}
